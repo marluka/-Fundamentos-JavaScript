59c32a0b271ddbbd448dbd8f373e3b760dfbf3d1
Marly Mejia <marly.mejia@gmail.com>
2020-06-24 23:30:42 +0200

    Clase 04: Variables: Strings

--- variables.js
+++ variables.js
@@ -1,3 +1,5 @@
+/* CLASE 3: Variables */
+
 var nombre = "Marly", apellido = "Mejia"
 var edad = 34
 
@@ -7,3 +9,18 @@ console.log('Hola soy ' + nombre + ' ' + apellido)
 console.log('Tengo ' + edad)
 
 var peso = 75
+
+
+/* CLASE 04: Variables: Strings */
+
+var nombreEnMayusculas = nombre.toUpperCase()
+var apellidoEnMinusculas = apellido.toLowerCase()
+
+var primeraLetraDelNombre = nombre.charAt(0)
+var cantidadDeLetrasDelNombre = nombre.length
+
+/* Interpolación de texto: ${código js} */
+var nombreCompleto = `${nombre} ${apellido.toUpperCase()}`
+
+/* obtener un substring */
+var str = nombre.substr(1, 2)
