9525d4ee3b124de66545f2331903191a539e5405
Marly Mejia <marly.mejia@gmail.com>
2020-06-26 00:39:05 +0200

    Clase 10: Parámetros como referencia o como valor

--- 06-objetos.js
+++ 06-objetos.js
@@ -10,6 +10,11 @@ var jaime = {
   edad: 28
 }
 
+/* 
+Las últimas versiones de JavaScript nos permiten desglosar el 
+objeto para acceder únicamente al atributo que nos interesa. 
+Esto se consigue encerrando el nombre del atributo entre llaves { }.
+*/
 function imprimirNombreEnMayusculas(persona) {
   // var nombre = persona.nombre -> es lo mismo q la siguiente linea
   var { nombre } = persona
@@ -21,6 +26,7 @@ imprimirNombreEnMayusculas(jaime)
 imprimirNombreEnMayusculas({ nombre: 'Pepito' })
 // imprimirNombreEnMayusculas({ apellido: 'Gomez' })
 
+
 /* RETO: crear una función que imprima: Hola me llamo ... y tengo ... años */
 
 function imprimirNombreYedad(persona) {
@@ -31,3 +37,21 @@ function imprimirNombreYedad(persona) {
 
 imprimirNombreYedad(pepe)
 imprimirNombreYedad(jaime)
+
+
+/* CLASE 10 Parámetros como referencia o como valor 
+ 
+Javascript se comporta de manera distinta cuando le pasamos un objeto como parámetro.
+Cuando los objetos se pasan como una referencia, estos se modifican fuera de la función.
+Se soluciona colocando tres puntos antes del nombre. Ej …persona.*/
+
+// function cumpleaños(persona){
+//   persona.edad += 1
+// }
+
+function cumpleaños(persona){
+  return {
+    ...persona,    /* ... crea una copia de persona */
+    edad: persona.edad + 1
+  }
+}
