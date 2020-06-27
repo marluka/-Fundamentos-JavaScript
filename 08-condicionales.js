678f5e4c6c71205d28a8cbe177065871fba27b90
Marly Mejia <marly.mejia@gmail.com>
2020-06-27 20:29:09 +0200

    Clase 14: Arrow functions

--- 08-condicionales.js
+++ 08-condicionales.js
@@ -43,24 +43,24 @@ function imprimirProfesiones(persona){
 
 }
 
-imprimirProfesiones(sacha)
+// imprimirProfesiones(sacha)
 
 
 /* RETO: Escribir una función imprimirSiEsMayorDeEdad */
 
-function imprimirSiEsMayorDeEdad(persona){
+// function imprimirSiEsMayorDeEdad(persona){
 
-  console.log(`${persona.nombre} es:`)
+//   console.log(`${persona.nombre} es:`)
 
-  if (persona.edad >= 18) {
-    console.log('mayor de edad')
-  }else{
-    console.log('menor de edad')
-  }
+//   if (persona.edad >= 18) {
+//     console.log('mayor de edad')
+//   }else{
+//     console.log('menor de edad')
+//   }
 
-}
+// }
 
-imprimirSiEsMayorDeEdad(sacha)
+// imprimirSiEsMayorDeEdad(sacha)
 
 
 /* CLASE 13: Funciones que retornan valores */
@@ -77,7 +77,7 @@ function esMayorDeEdad(persona) {
   return persona.edad >= MAYORIA_DE_EDAD
 }
 
-function imprimirSiEsMayorDeEdad2(persona) {
+function imprimirSiEsMayorDeEdad(persona) {
 
   if (esMayorDeEdad(persona)) {
     console.log(`${persona.nombre} es mayor de edad`)
@@ -87,4 +87,40 @@ function imprimirSiEsMayorDeEdad2(persona) {
 
 }
 
-imprimirSiEsMayorDeEdad2(juan)
+// imprimirSiEsMayorDeEdad(juan)
+
+
+/* CLASE 14: Arrow functions */
+
+/* funcion anonima */
+var esMayorDeEdad2 = function (persona) {
+  return persona.edad >= MAYORIA_DE_EDAD
+}
+
+// console.log (esMayorDeEdad2(sacha))
+
+/* Arrow function: equivalente a la anterior
+se puede omitir la palabra reservada function y las llaves */
+
+// const esMayorDeEdad3 = persona => persona.edad >= MAYORIA_DE_EDAD
+
+// desestructurando el objeto persona
+const esMayorDeEdad3 = ({edad}) => edad >= MAYORIA_DE_EDAD
+
+function permitirAcceso(persona){
+  if (!esMayorDeEdad3(persona)) {
+    console.log('ACCESO DENEGADO')
+  }
+}
+
+// permitirAcceso(sacha)
+// permitirAcceso(juan)
+
+
+/* RETO: Escribir una función esMenorDeEdad como arrow function 
+que devuelva la negación de esMayorDeEdad */
+
+const esMenorDeEdad = (persona) => ! esMayorDeEdad3(persona)
+
+
+
