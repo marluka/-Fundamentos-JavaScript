63f8075dada0d703770e74d156185fadb3f884f2
Marly Mejia <marly.mejia@gmail.com>
2020-06-29 19:40:33 +0200

    Clase 16: Estructuras repetitivas: while

--- 09-loop-for.js
+++ 09-loop-for.js
@@ -10,10 +10,17 @@ console.log(`Al inicio del año ${sacha.nombre} pesa ${sacha.peso}kg`)
 // function aumentarDePeso (persona) {
 //   return persona.peso += 200
 // }
-const INCREMENTO_PESO = 0.2
+const INCREMENTO_PESO = 0.3
 const DIAS_DEL_ANO = 365
 
+/* function aumentarDePeso(persona){
+  return persona.peso += INCREMENTO_PESO
+} */
 const aumentarDePeso = persona => persona.peso += INCREMENTO_PESO
+
+/* aumentarDePeso: Esta constante es equivalente a la función anterior.
+ => sin {} implicitamente retorna la sentencia que le sigue */
+
 const adelgazar = persona => persona.peso -= INCREMENTO_PESO
 
 for (var i = 1; i <= DIAS_DEL_ANO; i++) {
@@ -27,3 +34,26 @@ for (var i = 1; i <= DIAS_DEL_ANO; i++) {
 }
 
 console.log(`Al final del año ${sacha.nombre} pesa ${sacha.peso.toFixed(1)}kg`)
+
+
+
+/* ------------ CLASE 16: Loops while ------------ */
+
+const comeMucho = () => Math.random() < 0.3 
+const realizaDeporte = () => Math.random() < 0.4  
+
+const META = sacha.peso - 3
+var dias = 0
+
+while (sacha.peso > META) {
+  // debugger
+  if (comeMucho()) {
+    aumentarDePeso(sacha)
+  }
+  if (realizaDeporte()) {
+    adelgazar(sacha)
+  }
+  dias += 1
+}
+
+console.log(`Pasaron ${dias} días hasta que ${sacha.nombre} adelgazó 3kg`)
