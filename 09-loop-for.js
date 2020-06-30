ded45852ccaeedd130d2ce070af2717b07f58045
Marly Mejia <marly.mejia@gmail.com>
2020-06-30 16:09:27 +0200

    Clase 17: Estructuras repetitivas: do-whil

--- 09-loop-for.js
+++ 09-loop-for.js
@@ -57,3 +57,20 @@ while (sacha.peso > META) {
 }
 
 console.log(`Pasaron ${dias} días hasta que ${sacha.nombre} adelgazó 3kg`)
+
+
+/* ------------ CLASE 16: Estructuras repetitivas: do-while ------------ */
+
+var contador = 0
+
+const llueve = () => Math.random() < 0.25
+
+do {
+  contador++
+} while (!llueve())
+
+if (contador === 1) {
+  console.log(`Fui a ver si llovía ${contador} vez`)
+} else {
+  console.log(`Fui a ver si llovía ${contador} veces`)
+}
