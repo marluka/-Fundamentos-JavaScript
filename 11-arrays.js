c9a834a5de16b3ce8cb0c2b6416e5b916384e2ad
Marly Mejia <marly.mejia@gmail.com>
2020-06-30 20:42:24 +0200

    Clase 22: Reducir un array a un valor

--- 11-arrays.js
+++ 11-arrays.js
@@ -1,37 +1,43 @@
 var sacha = {
   nombre: 'Sacha',
   apellido: 'Lifszyc',
-  altura: 1.72
+  altura: 1.72,
+  cantidadDeLibros: 111
 }
 
 var alan = {
   nombre: 'Alan',
   apellido: 'Perez',
-  altura: 1.86
+  altura: 1.86,
+  cantidadDeLibros: 78
 }
 
 var martin = {
   nombre: 'Martin',
   apellido: 'Gomez',
-  altura: 1.85
+  altura: 1.85,
+  cantidadDeLibros: 132
 }
 
 var dario = {
   nombre: 'Dario',
   apellido: 'Juarez',
-  altura: 1.71
+  altura: 1.71,
+  cantidadDeLibros: 90
 }
 
 var vicky = {
   nombre: 'Vicky',
   apellido: 'Zapata',
-  altura: 1.56
+  altura: 1.56,
+  cantidadDeLibros: 91
 }
 
 var paula = {
   nombre: 'Paula',
   apellido: 'Barros',
-  altura: 1.76
+  altura: 1.76,
+  cantidadDeLibros: 182
 }
 
 var personas = [sacha, alan, martin, dario, vicky, paula]
@@ -60,3 +66,26 @@ const pasarAlturaACms = persona => ({
 var personasCms = personas.map(pasarAlturaACms)
 
 console.log(personasCms)
+
+
+/* CLASE 22: REDUCIR UN ARRAY A UN VALOR */
+
+/* 
+var acum = 0
+for (var i = 0; i < personas.length; i++) {
+  acum = acum + personas[i].cantidadDeLibros
+} 
+*/
+
+/* 
+const reducer = (acum, persona) => {
+  return acum + persona.cantidadDeLibros
+}
+
+ */
+
+const reducer = (acum, {cantidadDeLibros}) => acum + cantidadDeLibros
+
+var totalDeLibros = personas.reduce(reducer, 0)
+
+console.log(`En total todos tienen ${totalDeLibros} libros`)
