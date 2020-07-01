9bb5eee8848211725bf89b471a5b7ad58c21c7a0
Marly Mejia <marly.mejia@gmail.com>
2020-07-01 16:05:54 +0200

    Clase 23: Cómo funcionan las clases en JavaScript

---
+++ 12-prototipos.js
@@ -0,0 +1,26 @@
+
+function Persona(nombre, apellido, altura) {
+  // console.log('Me han ejecutado')
+  this.nombre = nombre
+  this.apellido = apellido
+  this.altura = altura
+}
+
+/* Dentro del prototipo Persona, va a existir un atributo  
+"saludar" que será una función, sin parametros */
+Persona.prototype.saludar = function () {
+  console.log(`Hola, me llamo ${this.nombre} ${this.apellido}`)
+}
+
+Persona.prototype.medidas = function () {
+  if (this.altura < 1.8) {
+    console.log(`Soy bajo, mido ${this.altura}`)
+  } else {
+    console.log(`Soy alto, mido ${this.altura}`)
+  }
+}
+
+
+var sacha = new Persona('Sacha', 'Lifszyc', 1.72)
+var erika = new Persona('Erika', 'Luna', 1.56)
+var arturo = new Persona('Arturo', 'Martinez', 1.86)
