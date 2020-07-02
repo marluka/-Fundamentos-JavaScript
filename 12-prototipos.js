0035c15d9711a8c863efde3f291c99fa89eb484d
Marly Mejia <marly.mejia@gmail.com>
2020-07-02 15:55:38 +0200

    Clase 27: Clases en JavaScript

--- 12-prototipos.js
+++ 12-prototipos.js
@@ -1,47 +1,29 @@
+class Persona {
+  constructor(nombre, apellido, altura) {
+    this.nombre = nombre
+    this.apellido = apellido
+    this.altura = altura
+  }
 
-function heredaDe(prototipoHijo, prototipoPadre){
-  var fn = function () {}
-  fn.prototype = prototipoPadre.prototype
-  prototipoHijo.prototype = new fn
-  prototipoHijo.prototype.constructor = prototipoHijo
-}
+  saludar() {
+    console.log(`Hola, me llamo ${this.nombre} ${this.apellido}`)
+  }
 
-function Persona(nombre, apellido, altura) {
-  // console.log('Me han ejecutado')
-  this.nombre = nombre
-  this.apellido = apellido
-  this.altura = altura
-}
-
-/* Dentro del prototipo Persona, va a existir un atributo  
-"saludar" que será una función, sin parametros */
-Persona.prototype.saludar = function () {
-  console.log(`Hola, me llamo ${this.nombre} ${this.apellido}`)
-}
-
-Persona.prototype.medidas = function () {
-  if (this.altura < 1.8) {
-    console.log(`Soy bajo, mido ${this.altura}`)
-  } else {
-    console.log(`Soy alto, mido ${this.altura}`)
+  soyAlto() {
+    return this.altura > 1.8
   }
 }
 
-Persona.prototype.soyAlto = function () {
-  return this.altura > 1.8
+class Desarrollador extends Persona {
+  constructor(nombre, apellido, altura) {
+    super(nombre, apellido, altura)
+  }
+
+  saludar() {
+    console.log(`Hola, me llamo ${this.nombre} ${this.apellido} soy desarrollador web"`)
+  }
 }
 
-function Desarrollador(nombre, apellido) {
-  this.nombre = nombre
-  this.apellido = apellido
-}
-
-heredaDe(Desarrollador, Persona)
-
-/* se machaca el prototipo saludar heredado */
-Desarrollador.prototype.saludar = function() {
-  console.log(`Hola, me llamo ${this.nombre} ${this.apellido} soy desarrollador web"`)
-}
 
 // var sacha = new Persona('Sacha', 'Lifszyc', 1.72)
 // var erika = new Persona('Erika', 'Luna', 1.56)
