dd8f6e687262f13289718878c2faf0661b3c5a2a
Marly Mejia <marly.mejia@gmail.com>
2020-07-03 12:15:57 +0200

    Clase 33: Haciendo múltiples requests

--- 13-callbacks.js
+++ 13-callbacks.js
@@ -1,18 +1,18 @@
 /* Hacer request */
 const API_URL = 'https://swapi.dev/api/'
 const PEOPLE_URL = 'people/:id'
-
-const lukeUrl = `${API_URL}${PEOPLE_URL.replace(':id', 1)}`
 const options = { crossDomain: true }
 
-/* callback: una función que se ejecutará en el futuro, 
-y q $.get será el encargado de llamarla cuando termine el request*/
-
 const onResponse = function(data){
-  /* arguments: es una variable que tienen todas las funciones
-  y nos da un array con todos los parámetros q recibe la función */
-  // console.log(arguments)
-  console.log(`Hola yo soy ${data.name}`)
+  console.log(`Hola, yo soy ${data.name}`)
 }
 
-$.get(lukeUrl, options, onResponse)
+function obtenerPersonaje(id){
+  const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`
+  $.get(url, options, onResponse)
+}
+
+/* Debido al asincrinosmo de js no sabemos en que orden nos van a llegar las respuestas */
+obtenerPersonaje(1)
+obtenerPersonaje(2)
+obtenerPersonaje(3)
