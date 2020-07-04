7abc6ec4b24eefe66b72cfd4a6b0c04a6e441694
Marly Mejia <marly.mejia@gmail.com>
2020-07-04 19:04:10 +0200

    Clase 35: Manejo de errores con callbacks

--- 13-callbacks.js
+++ 13-callbacks.js
@@ -6,22 +6,31 @@ const options = { crossDomain: true }
 function obtenerPersonaje(id, callback){
   const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`
 
-  $.get(url, options, function(data){
-    console.log(`Hola, yo soy ${data.name}`)
-    if (callback) {
-      callback()
-    }
+  $.get(url, options, callback).fail(()=> {
+    console.log(`Sucedió un error. No se pudo obtener el personaje ${id}`)
   })
 }
 /* para ganrantizar el orden de las respuestas 
 utilizamos callbacks*/
 
-obtenerPersonaje(1, function(){
-  obtenerPersonaje(2, function(){
-    obtenerPersonaje(3, function(){
-      obtenerPersonaje(4,function(){
-        obtenerPersonaje(5,function(){
-          obtenerPersonaje(6)
+obtenerPersonaje(1, function(personaje){
+  console.log(`Hola, yo soy ${personaje.name}`)
+
+  obtenerPersonaje(2, function(personaje){
+    console.log(`Hola, yo soy ${personaje.name}`)
+
+    obtenerPersonaje(3, function(personaje){
+      console.log(`Hola, yo soy ${personaje.name}`)
+
+      obtenerPersonaje(4,function(personaje){
+        console.log(`Hola, yo soy ${personaje.name}`)
+
+        obtenerPersonaje(5,function(personaje){
+          console.log(`Hola, yo soy ${personaje.name}`)
+
+          obtenerPersonaje(6,function(personaje){
+            console.log(`Hola, yo soy ${personaje.name}`)
+          })
         })
       })
     })
