2797fc18b0e0ae4efef3f291e2aed7d60a67a04d
Marly Mejia <marly.mejia@gmail.com>
2020-07-05 23:53:01 +0200

    Clase 37: Promesas encadenadas

--- 13-callbacks.js
+++ 13-callbacks.js
@@ -4,7 +4,7 @@ const PEOPLE_URL = 'people/:id'
 const options = { crossDomain: true }
 
 function obtenerPersonaje(id){
-  return new Promise((resolve, reject)=> {
+  return new Promise((resolve, reject) => {
     const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`
     $.get(url, options, function(data){
       resolve(data)
@@ -18,9 +18,35 @@ function onError(id) {
 }
 
 
-obtenerPersonaje(1).then(function(personaje){
+obtenerPersonaje(1)
+.then(personaje => {
   console.log(`El personaje 1 es ${personaje.name}`)
-}).catch(onError)
+  return obtenerPersonaje(2)
+})
+.then(personaje => {
+  console.log(`El personaje 2 es ${personaje.name}`)
+  return obtenerPersonaje(3)
+})
+.then(personaje => {
+  console.log(`El personaje 3 es ${personaje.name}`)
+  return obtenerPersonaje(4)
+})
+.then(personaje => {
+  console.log(`El personaje 4 es ${personaje.name}`)
+  return obtenerPersonaje(5)
+})
+.then(personaje => {
+  console.log(`El personaje 5 es ${personaje.name}`)
+  return obtenerPersonaje(6)
+})
+.then(personaje => {
+  console.log(`El personaje 6 es ${personaje.name}`)
+  return obtenerPersonaje(7)
+})
+.then(personaje => {
+  console.log(`El personaje 7 es ${personaje.name}`)
+})
+.catch(onError)
 
 
 
