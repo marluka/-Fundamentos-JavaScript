fc5fd63e2b74c818ae11a1ddb63f8b0107f1e798
Marly Mejia <marly.mejia@gmail.com>
2020-07-05 23:44:34 +0200

    Clase 36: Promesas

--- 13-callbacks.js
+++ 13-callbacks.js
@@ -3,37 +3,24 @@ const API_URL = 'https://swapi.dev/api/'
 const PEOPLE_URL = 'people/:id'
 const options = { crossDomain: true }
 
-function obtenerPersonaje(id, callback){
-  const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`
-
-  $.get(url, options, callback).fail(()=> {
-    console.log(`Sucedió un error. No se pudo obtener el personaje ${id}`)
+function obtenerPersonaje(id){
+  return new Promise((resolve, reject)=> {
+    const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`
+    $.get(url, options, function(data){
+      resolve(data)
+    }).fail(()=> reject(id))
   })
 }
-/* para ganrantizar el orden de las respuestas 
-utilizamos callbacks*/
 
-obtenerPersonaje(1, function(personaje){
-  console.log(`Hola, yo soy ${personaje.name}`)
 
-  obtenerPersonaje(2, function(personaje){
-    console.log(`Hola, yo soy ${personaje.name}`)
+function onError(id) {
+  console.log(`Sucedió un error al obtener el personaje ${id}`)
+}
 
-    obtenerPersonaje(3, function(personaje){
-      console.log(`Hola, yo soy ${personaje.name}`)
 
-      obtenerPersonaje(4,function(personaje){
-        console.log(`Hola, yo soy ${personaje.name}`)
+obtenerPersonaje(1).then(function(personaje){
+  console.log(`El personaje 1 es ${personaje.name}`)
+}).catch(onError)
 
-        obtenerPersonaje(5,function(personaje){
-          console.log(`Hola, yo soy ${personaje.name}`)
 
-          obtenerPersonaje(6,function(personaje){
-            console.log(`Hola, yo soy ${personaje.name}`)
-          })
-        })
-      })
-    })
-  })
-})
 
