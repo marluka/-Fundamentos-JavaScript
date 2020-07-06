348f56b31eeacbf8e28192faf0ff7e650b6e1be8
Marly Mejia <marly.mejia@gmail.com>
2020-07-06 17:09:06 +0200

    Clase 38: Múltiples promesas en paralelo

--- 13-callbacks.js
+++ 13-callbacks.js
@@ -18,35 +18,47 @@ function onError(id) {
 }
 
 
-obtenerPersonaje(1)
-.then(personaje => {
-  console.log(`El personaje 1 es ${personaje.name}`)
-  return obtenerPersonaje(2)
-})
-.then(personaje => {
-  console.log(`El personaje 2 es ${personaje.name}`)
-  return obtenerPersonaje(3)
-})
-.then(personaje => {
-  console.log(`El personaje 3 es ${personaje.name}`)
-  return obtenerPersonaje(4)
-})
-.then(personaje => {
-  console.log(`El personaje 4 es ${personaje.name}`)
-  return obtenerPersonaje(5)
-})
-.then(personaje => {
-  console.log(`El personaje 5 es ${personaje.name}`)
-  return obtenerPersonaje(6)
-})
-.then(personaje => {
-  console.log(`El personaje 6 es ${personaje.name}`)
-  return obtenerPersonaje(7)
-})
-.then(personaje => {
-  console.log(`El personaje 7 es ${personaje.name}`)
-})
+var ids = [1, 2, 3, 4, 5, 6, 7]
+
+// var promesas = ids.map(function(id){
+//   return obtenerPersonaje(id)
+// })
+
+var promesas = ids.map(id => obtenerPersonaje(id))
+Promise
+.all(promesas)
+.then(personajes => console.log(personajes))
 .catch(onError)
+
+// obtenerPersonaje(1)
+// .then(personaje => {
+//   console.log(`El personaje 1 es ${personaje.name}`)
+//   return obtenerPersonaje(2)
+// })
+// .then(personaje => {
+//   console.log(`El personaje 2 es ${personaje.name}`)
+//   return obtenerPersonaje(3)
+// })
+// .then(personaje => {
+//   console.log(`El personaje 3 es ${personaje.name}`)
+//   return obtenerPersonaje(4)
+// })
+// .then(personaje => {
+//   console.log(`El personaje 4 es ${personaje.name}`)
+//   return obtenerPersonaje(5)
+// })
+// .then(personaje => {
+//   console.log(`El personaje 5 es ${personaje.name}`)
+//   return obtenerPersonaje(6)
+// })
+// .then(personaje => {
+//   console.log(`El personaje 6 es ${personaje.name}`)
+//   return obtenerPersonaje(7)
+// })
+// .then(personaje => {
+//   console.log(`El personaje 7 es ${personaje.name}`)
+// })
+// .catch(onError)
 
 
 
